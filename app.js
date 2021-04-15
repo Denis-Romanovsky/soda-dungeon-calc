3680da955740f7502b1bbcefc2b6595370c3089a
Denis Romanovsky <steam.pwrd@yandex.ru>
2021-04-15 09:19:00 +0300

    15/04
    - Added different kinds of relics
    - Created tabs for relics
    - Changed the design of the app
    - Removed redundant code
    - Additional small changes

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const levelInput = document.querySelectorAll('.level-input');
+const levelInput = document.querySelectorAll('.relics--card--stat__level-input');
 const resetBtn = document.querySelectorAll('.btn-delete');
 const resetAll = document.querySelector('.btn-delete-all');
-const form = document.querySelector('.relic-form');
-const saveData = document.querySelector('#saveData');
+const relicCard = document.querySelectorAll('.relics--card');
+const relicName = document.querySelectorAll('.relics--card__name');
+const hideRelicNames = document.querySelector('#hideRelicNames');
 
 // used for limiting relic level to a certain value
 const relicLevelCapped = document.querySelectorAll('.restricted');
 
+const menu = document.querySelector('.menu');
+const menuToggleButton = document.querySelector('.menu--toggle');
+
+// const saveData = document.querySelector('#saveData');
 
 // ------------------------ RELIC TEMPLATE ------------------------ \\
 class Relic {
@@ -18,8 +23,8 @@ class Relic {
     this.shortDescription = shortDescription
   }
   value() {
-    // let result = Math.round((this.level *= this.multiplier) * 100) / 100;
-    return Math.floor(this.level *= this.multiplier);
+    // let result = Math.round((this.level * this.multiplier) * 100) / 100;
+    return Math.floor(this.level * this.multiplier);
   }
 };
 
@@ -45,6 +50,27 @@ const critDmgRelic = new RelicCapped('critDmgRelic', 1, 1, 0.5,'% Crit Damage', 
 const essenceRelic = new RelicCapped('essenceRelic', 1, 1, 0.2,'% Essence Find', 100);
 const regenRelic = new RelicCapped('regenRelic', 1, 1, 1,' HP Regen', 100);
 
+// Secondary Relics(all capped)
+const dungeonKeysRelic = new RelicCapped('dungeonKeysRelic', 1, 1, 0.2,'% Chance of Finding Dungeon Keys', 300);
+const masteryRelic = new RelicCapped('masteryRelic', 1, 1, 0.1,'% Boost to Character Mastery XP', 100);
+const oreRelic = new RelicCapped('oreRelic', 1, 1, 0.1,' Ore Find', 100);
+const resistRelic = new RelicCapped('resistRelic', 1, 1, 0.25,'% Status Resist', 52);
+const reflectionRelic = new RelicCapped('reflectionRelic', 1, 1, 0.2,'%  Damage Reflection', 100);
+
+// Class Relics
+const junkieRelic = new Relic('junkieRelic', 1, 1, 2,' ATK/HP/MP');
+const carpenterRelic = new Relic('carpenterRelic', 1, 1, 2,' ATK/HP/MP');
+const minerRelic = new Relic('minerRelic', 1, 1, 2,' ATK/HP/MP');
+const nurseRelic = new Relic('nurseRelic', 1, 1, 2,' ATK/HP/MP');
+const mysticRelic = new Relic('mysticRelic', 1, 1, 2,' ATK/HP/MP');
+const thiefRelic = new Relic('thiefRelic', 1, 1, 2,' ATK/HP/MP');
+const huntressRelic = new Relic('huntressRelic', 1, 1, 2,' ATK/HP/MP');
+const darkmageRelic = new Relic('darkmageRelic', 1, 1, 2,' ATK/HP/MP');
+const blademasterRelic = new Relic('blademasterRelic', 1, 1, 2,' ATK/HP/MP');
+const darkLordRelic = new Relic('darkLordRelic', 1, 1, 2,' ATK/HP/MP');
+const chefRelic = new Relic('chefRelic', 1, 1, 2,' ATK/HP');
+const wizardRelic = new Relic('wizardRelic', 1, 1, 2,' ATK/HP/MP');
+const blacksmithRelic = new Relic('blacksmithRelic', 1, 1, 2,' ATK/HP/MP');
 
 // Array of relics
 const mainRelics = [
@@ -60,33 +86,137 @@ const mainRelics = [
   critChanceRelic,
   critDmgRelic,
   essenceRelic,
-  regenRelic
+  regenRelic,
+
+  // secondaryRelics
+  dungeonKeysRelic,
+  masteryRelic,
+  oreRelic,
+  resistRelic,
+  reflectionRelic,
+
+  // Class Relics
+  junkieRelic,
+  carpenterRelic,
+  minerRelic,
+  nurseRelic,
+  mysticRelic,
+  thiefRelic,
+  huntressRelic,
+  darkmageRelic,
+  blademasterRelic,
+  darkLordRelic,
+  chefRelic,
+  wizardRelic,
+  blacksmithRelic
 ];
 
 // LOCAL STORAGE
-localStorage.getItem('myKey');
+let relicKeys = [];
+let relicValues = [];
 
+// Initialize localStorage and also use this as a template for updating the values
+const setLocalStorage = () => {
+
+  // push values to arrays that store keys/values
+  for (let eachRelic of mainRelics) {
+    relicKeys.push(eachRelic.name);
+    relicValues.push(eachRelic.level);
+  }
+
+  // make an object out of them
+  let result = relicKeys.reduce((result, value, index) => {
+    result[value] = relicValues[index];
+    return result;
+  }, {})
+
+  // create a key for localStorage
+  const relicStorage = JSON.stringify(result);
+  return localStorage.setItem('relics', relicStorage);
+};
+
+
+if (localStorage.length === 0) {
+  setLocalStorage();
+}
+
+const relicStored = JSON.parse(localStorage.getItem('relics'))
+localStorage.getItem('relics', JSON.stringify(relicStored))
+
+
+// ---------------------- Website UI Related Stuff ----------------------
+const toggleMenu = () => {
+  if (menu.classList.contains('active')) {
+    menu.classList.remove('active')
+  } else {
+    menu.classList.add('active');
+  }
+}
+
+menuToggleButton.addEventListener('click', toggleMenu);
+
+
+// It's for creating tabs. Got this from https://css-tricks.com/tabs-its-complicated/
+// codepen https://codepen.io/chriscoyier/pen/XQpqZV
+const tabs = document.querySelectorAll('.main--tabs .tabs li');
+const sections = document.querySelectorAll('.main--tabs .tab-content');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', e => {
+    e.preventDefault();
+    removeActiveTab();
+    addActiveTab(tab);
+  });
+})
+
+const removeActiveTab = () => {
+  tabs.forEach(tab => {
+    tab.classList.remove('is-active');
+  });
+  sections.forEach(section => {
+    console.log(section);
+    section.classList.remove('is-active');
+  });
+}
+
+const addActiveTab = tab => {
+  tab.classList.add('is-active');
+  const href = tab.querySelector('a').getAttribute('href');
+  const matchingSection = document.querySelector(href);
+  matchingSection.classList.add('is-active');
+}
+
+
+
+// ---------------------- LOGIC PART OF THE CODE ----------------------
 
 // INPUT FIELD CALCULATION. RECEIVE A VALUE IN ORDER TO CALCULATE IT FOR RELIC BONUS DESCRIPTION
 // --------------  REFACTORED --------------
 levelInput.forEach(item => {
 
-  // #################################### LOAD FORM LOCAL STORAGE HERE
+  // relic-bonus of each relic class
+  let relicDescription = item.nextElementSibling.nextElementSibling;
 
   for (let relic of mainRelics) {
-    // relic-bonus of each relic class
-    let relicDescription = item.nextElementSibling.nextElementSibling;
 
-    // if input is empty set description to its relic multiplier and relic description, basically set it to minLevel
-    if (item.value.length == 0) {
+    // LOCAL STORAGE
+    if (relicStored.hasOwnProperty(relic.name) && item.id === relic.name) {
+      relic.level = relicStored[relic.name];
+      item.value = relic.level
+      relicDescription.innerText = `+${relic.value()}${relic.shortDescription}`;
+    }
+
+    // set to empty because there number 1 in the input field, meaning if I type a number it will always be 142 i.e 1(the first index) won't be removed
+    if (item.value.length == 0 || relic.level == 1) {
       if (item.id == relic.name) {
+        item.value = '';
+        // if input is empty set description to its relic multiplier and relic description, basically set it to minLevel
         relicDescription.innerText = `+${relic.multiplier}${relic.shortDescription}`;
       };
     };
 
     // update descriptions in real time based on input
     item.addEventListener('input', updateValue => {
-
       // get access to each relic-bonus to edit them individually if item.id is equal to relic.name
       // insert values into specific relic description if true
       if (item.id == relic.name) {
@@ -97,12 +227,9 @@ levelInput.forEach(item => {
 
         // set relic description
         relicDescription.innerText = `+${relic.value()}${relic.shortDescription}`;
-
-        // update relic level so it doesn't get the value of relic.value(). Actually, I should change the code, so I don't use relic.level = ... before the if statement, cause that's what caused the bug
-        relic.level = parseInt(updateValue.target.value);
-        // remove NaN level. Damn I really gotta sort this thing out. ...but later
-        if (updateValue.target.value.length === 0) { relic.level = 1; };
+        console.log(relic.value());
       };
+      setLocalStorage(); // Update localStorage in real time
     });
   };
 });
@@ -120,12 +247,11 @@ resetBtn.forEach(btn => {
         e.target.previousElementSibling.value = ''; // empty the input field
         // also set the bonus value to its multiplier as base(by default when lvl 1)
         relicDescription.innerText = `+${relic.multiplier}${relic.shortDescription}`;
+        setLocalStorage(); // RESET LOCAL STORAGE;
       }
-      localStorage.setItem(JSON.stringify(relic.name), JSON.stringify(relic.level));
     }
   })
 })
-
 
 
 // RESET ALL BUTTON
@@ -141,17 +267,16 @@ resetAll.addEventListener('click', () => {
       if (elem.id == relic.name) {
         relic.level = 1;
         relicDescription.innerText = `+${relic.multiplier}${relic.shortDescription}`
+        setLocalStorage(); // RESET LOCAL STORAGE
       }
     });
-
-    // RESET LOCAL STORAGE
-    localStorage.setItem(JSON.stringify(relic.name), JSON.stringify(relic.level));
   }
 });
 
 
 // CHANGING INPUT VALUE IF IT BECOMES MORE THAN relicClass.maxLevel
 relicLevelCapped.forEach(elemCapped => {
+  // Get the description of each relic
   let relicDescription = elemCapped.nextElementSibling.nextElementSibling;
 
   for (let relic of mainRelics) {
@@ -163,6 +288,7 @@ relicLevelCapped.forEach(elemCapped => {
 
           // update the relic.level value
           relic.level = relic.maxLevel
+          setLocalStorage(); // Update localStorage to prevent lvl going past the limit (100+/250+)
         }
       }
     })
@@ -171,28 +297,22 @@ relicLevelCapped.forEach(elemCapped => {
 
 
 // SAVE TO LOCAL STORAGE
-saveData.addEventListener('click', () => {
-  let relicKeys = [];
-  let relicValues = [];
+// saveData.addEventListener('click', () => {
+//   if (localStorage.length !== 0) {
+//     setLocalStorage();
+//   }
+// });
 
-  for (let relicData of mainRelics) {
-    relicKeys.push(relicData.name);
-    relicValues.push(relicData.level);
-  }
 
-  let result = relicKeys.reduce((result, value, index) => {
-    result[value] = relicValues[index];
-    return result;
-  }, {})
-  console.log(result);
-  const relicStorage = JSON.stringify(result);
-  localStorage.setItem('relics', JSON.stringify(relicStorage));
-});
-
+// Toggle relic names
+hideRelicNames.addEventListener('click', () => {
+  relicName.forEach(name => {
+    name.classList.toggle('hide-full');
+  })
+})
 
 // onmouseover="test()
 // const test = () => {
 //   // console.log('s');
 //   console.log(testtest);
 // };
-
